06d6c9978e5d79a7f24ff1dfbb38abf62d0f78a7
mahfuzders <mahfuzders@gmail.com>
2025-12-14 14:11:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,6 @@ app.listen(PORT, () => {
 
 const SERVER_HOST = 'iamsofiathefirsttt.aternos.me';
 const SERVER_PORT = 25565;
-const BOT_USERNAME = 'Bot' + Math.floor(Math.random() * 10000);
 let botActive = false;
 
 function createBot() {
@@ -22,7 +21,11 @@ function createBot() {
     return;
   }
   
+  // Her seferinde YENİ RANDOM İSİM
+  const BOT_USERNAME = 'Bot' + Math.floor(Math.random() * 100000);
+  
   console.log('\n=== YENi BOT OLUSTURULUYOR ===');
+  console.log('Bot ismi: ' + BOT_USERNAME);
   console.log('Sunucu: ' + SERVER_HOST + ':' + SERVER_PORT);
   
   botActive = true;
@@ -38,12 +41,18 @@ function createBot() {
   });
   
   bot.on('login', () => {
-    console.log('>>> BOT OYUNA GIRDI! <<<');
+    console.log('>>> BOT OYUNA GIRDI: ' + BOT_USERNAME + ' <<<');
     console.log('Konum: ' + bot.entity.position);
+    
+    // 1 dakika sonra botu çıkar
+    setTimeout(() => {
+      console.log('1 dakika doldu, ' + BOT_USERNAME + ' cikiyor...');
+      bot.quit();
+    }, 60000);
   });
   
   bot.on('spawn', () => {
-    console.log('Bot spawn oldu!');
+    console.log(BOT_USERNAME + ' spawn oldu!');
   });
   
   bot.on('chat', (username, message) => {
@@ -51,21 +60,21 @@ function createBot() {
   });
   
   bot.on('kicked', (reason) => {
-    console.log('Bot kicklendi: ' + reason);
+    console.log(BOT_USERNAME + ' kicklendi: ' + reason);
     botActive = false;
-    setTimeout(createBot, 5000);
+    setTimeout(createBot, 60000);
   });
   
   bot.on('error', (err) => {
-    console.log('Bot hatasi: ' + err.message);
+    console.log(BOT_USERNAME + ' hatasi: ' + err.message);
     botActive = false;
-    setTimeout(createBot, 5000);
+    setTimeout(createBot, 60000);
   });
   
   bot.on('end', () => {
-    console.log('Bot baglantisi kapandi');
+    console.log(BOT_USERNAME + ' baglantisi kapandi');
     botActive = false;
-    setTimeout(createBot, 5000);
+    setTimeout(createBot, 60000);
   });
 }
 
