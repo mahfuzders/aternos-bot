d73b1a419ca75d66e3f75f80a40473a90aea74ce
mahfuzders <mahfuzders@gmail.com>
2025-12-14 12:49:07 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,89 @@
+const mineflayer = require('mineflayer');
+const express = require('express');
+
+const app = express();
+const PORT = 10000;
+
+app.get('/', (req, res) => {
+  res.send('Aternos Minecraft Bot Calisiyor!');
+});
+
+app.listen(PORT, () => {
+  console.log('Web sunucu calisiyor: ' + PORT);
+});
+
+const SERVER_HOST = 'iamsofiathefirsttt.aternos.me';
+const SERVER_PORT = 25565;
+const BOT_USERNAME = 'AternosBot';
+
+let botActive = false;
+
+function createBot() {
+  if (botActive) {
+    console.log('Bot zaten aktif, bekleniyor...');
+    return;
+  }
+
+  console.log('\n=== YENi BOT OLUSTURULUYOR ===');
+  console.log('Sunucu: ' + SERVER_HOST + ':' + SERVER_PORT);
+  
+  botActive = true;
+
+  const bot = mineflayer.createBot({
+    host: SERVER_HOST,
+    port: SERVER_PORT,
+    username: BOT_USERNAME,
+    version: false,
+    auth: 'offline'
+  });
+
+  bot.on('login', () => {
+    console.log('>>> BOT OYUNA GIRDI! <<<');
+    console.log('Konum: ' + bot.entity.position);
+    
+    setTimeout(() => {
+      console.log('30 saniye doldu, bot cikis yapiyor...');
+      bot.quit();
+    }, 30000);
+  });
+
+  bot.on('spawn', () => {
+    console.log('Bot spawn oldu!');
+  });
+
+  bot.on('chat', (username, message) => {
+    console.log('Chat: <' + username + '> ' + message);
+  });
+
+  bot.on('kicked', (reason) => {
+    console.log('Bot kicklendi: ' + reason);
+    botActive = false;
+  });
+
+  bot.on('error', (err) => {
+    console.log('Bot hatasi: ' + err.message);
+    botActive = false;
+  });
+
+  bot.on('end', () => {
+    console.log('Bot baglantisi kapandi');
+    botActive = false;
+  });
+}
+
+console.log('=== ATERNOS MINECRAFT BOT BASLADI ===');
+
+setTimeout(() => {
+  console.log('Ilk bot 20 saniye sonra baslatiliyor...');
+}, 1000);
+
+setTimeout(() => {
+  createBot();
+}, 20000);
+
+setInterval(() => {
+  if (!botActive) {
+    console.log('\n--- Yeni baglanti denemesi (60 saniye sonra) ---');
+    createBot();
+  }
+}, 60000);
